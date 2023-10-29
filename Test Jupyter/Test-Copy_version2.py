b96b722d0a55fd41951d1c2cdbb1e67940b8a155
Emrah Fidan <emrahfdn.work@gmail.com>
2023-10-29 13:32:38 +0300

    initial commit

---
+++ Test Jupyter/Test-Copy_version2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+print("test")
+
+
+# In[2]:
+
+
+x = 10
+print(x)
+
+
+# <H1>Hello World</H1>
+
+# In[5]:
+
+
+for item in range (1,11):
+    print(item)
+
+
+# ### for döngüleri ile ilgili ...
+
+# In[ ]:
+
+
+
+
